27f9f66a8bcd70093a58d34bfde2ddc64bc07139
Neetsonic <Neetsonic@gmail.com>
2017-06-14 16:47:43 +0800

    3

--- Neetsonic/Tool/Database/SqlExecutor.cs
+++ Neetsonic/Tool/Database/SqlExecutor.cs
@@ -1,7 +1,12 @@
-﻿namespace Neetsonic.Tool.Database
+﻿using System.Windows.Forms;
+
+namespace Neetsonic.Tool.Database
 {
     internal class SqlExecutor
     {
-        public void Fun() { }
+        public void Fun()
+        {
+            MessageBox.Show(@"撒旦");
+        }
     }
 }
