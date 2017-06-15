d2e3a2090dc9eda2146c39284f843b15b89febc5
neetsonic <neetsonic@gmail.com>
2017-06-15 21:38:27 +0800

    创建了SqlExecutor

--- Neetsonic/Tool/Database/SqlExecutor.cs
+++ Neetsonic/Tool/Database/SqlExecutor.cs
@@ -1,12 +1,86 @@
-﻿using System.Windows.Forms;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace Neetsonic.Tool.Database
 {
-    internal class SqlExecutor
+    /// <summary>
+    /// Sql执行器
+    /// Neetsonic
+    /// 2017.6.15
+    /// </summary>
+    public static class SqlExecutor
     {
-        public void Fun()
+        /// <summary>
+        /// 执行不带返回值的Sql语句
+        /// </summary>
+        /// <param name="connection">数据库连接字符串</param>
+        /// <param name="sql">要执行的sql语句</param>
+        public static void ExecuteNonQuery(string connection, string sql)
         {
-            MessageBox.Show(@"撒旦");
+            using(SqlConnection conn = new SqlConnection(connection))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// 执行单一返回值的Sql语句
+        /// </summary>
+        /// <param name="connection">数据库连接字符串</param>
+        /// <param name="sql">要执行的sql语句</param>
+        /// <returns>单一返回值</returns>
+        public static object ExecuteScalar(string connection, string sql)
+        {
+            using(SqlConnection conn = new SqlConnection(connection))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                return cmd.ExecuteScalar();
+            }
+        }
+
+        /// <summary>
+        /// 执行带有返回值的Sql语句
+        /// </summary>
+        /// <param name="connection">数据库连接字符串</param>
+        /// <param name="sql">要执行的sql语句</param>
+        /// <returns>返回值</returns>
+        public static DataSet ExecuteQuery(string connection, string sql)
+        {
+            using(SqlConnection conn = new SqlConnection(connection))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                using(SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                {
+                    DataSet dataSet = new DataSet();
+                    adapter.Fill(dataSet);
+                    return dataSet;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 测试数据库连接是否成功
+        /// </summary>
+        /// <param name="connection">数据库连接字符串</param>
+        /// <returns>连接是否成功</returns>
+        public static bool ConnectSucceed(string connection)
+        {
+            try
+            {
+                using(SqlConnection conn = new SqlConnection())
+                {
+                    conn.Open();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
