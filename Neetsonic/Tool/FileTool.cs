3d9b1c206b57b99422e10a9495f74f252fa11887
neetsonic <neetsonic@gmail.com>
2017-08-04 20:29:34 +0800

    dev

---
+++ Neetsonic/Tool/FileTool.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Neetsonic.Tool
+{
+    /// <summary>
+    /// 文件处理工具
+    /// </summary>
+    public static class FileTool
+    {
+        /// <summary>
+        /// 批量删除文件
+        /// </summary>
+        /// <param name="files">文件路径集合</param>
+        public static void DeleteFiles(IEnumerable<string> files)
+        {
+            foreach(string file in files)
+            {
+                File.Delete(file);
+            }
+        }
+    }
+}
---
+++ Neetsonic/Tool/SqlParameterEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Neetsonic.Tool
+{
+    /// <summary>
+    /// SqlParameter扩展方法
+    /// </summary>
+    public static class SqlParameterEx
+    {
+        /// <summary>
+        /// 可以为空的数据库字符串
+        /// </summary>
+        /// <param name="str">源字符串</param>
+        /// <returns>数据库字符串</returns>
+        public static object NullableString(string str)
+        {
+            if(null == str)
+            {
+                return DBNull.Value;
+            }
+            return str;
+        }
+    }
+}
