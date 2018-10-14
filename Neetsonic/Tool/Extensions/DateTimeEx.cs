47aafcd35abe5ca65c7dfdcb50e8239fb00bb37d
neetsonic <neetsonic@gmail.com>
2018-10-15 00:47:25 +0800

    dev

---
+++ Neetsonic/Tool/Extensions/DateTimeEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Neetsonic.Tool.Extensions
+{
+    /// <summary>
+    ///  DateTime扩展类
+    /// </summary>
+    public static class DateTimeEx
+    {
+        /// <summary>
+        /// 获取从UTC 1970年以来经过的毫秒数
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static long Utc1970InMillisec(this DateTime date) =>(date.Ticks - 621355968000000000L) / 10000;
+    }
+}
